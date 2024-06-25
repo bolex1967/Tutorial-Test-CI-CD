10f22240065bb3f2996d3d45e655887d2e7b6c5a
Olexander B <bolex1967@gmail.com>
2024-06-25 11:45:59 +0300

    Created basic project structure

---
+++ add.js
@@ -0,0 +1,8 @@
+// add.js
+// The function of adding two numbers.
+
+function add(a, b) {
+  return a + b;
+}
+
+module.exports = add; // Експортуємо функцію 'add'
---
+++ readme-git.txt
@@ -0,0 +1,5 @@
+git init
+git status
+git add filename
+git rm --cashed filename // Повернення назад в стадію modify
+git add .
